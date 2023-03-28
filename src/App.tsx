cbcb7627560ac49e0ea30fb38a87e864fcf74c13
MasterMong <mongkon@outlook.com>
2023-03-28 17:13:03 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <h4>ระบบบันทึกการจัดการเรียนการสอน</h4>
+        <p>
+          เปิดใช้งานสัปดาห์หน้าครับ
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
